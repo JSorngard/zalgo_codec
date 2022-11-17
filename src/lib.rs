4a47eaedd8f3e064f9b5cb2c5aaf758a3f35aae8
Johanna Sörngård <jsorngard@gmail.com>
2022-11-17 18:51:24 +0100

    mention reversibility of encoding in crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! originally written in Python by [Scott Conner](https://github.com/DaCoolOne/DumbIdeas/tree/main/reddit_ph_compressor).
 //!
 //! Using the functions defined here you can transform an ASCII string into a unicode string that is a single
-//! "character" wide. This string will almost surely be larger than the original in terms of bytes.
+//! "character" wide. This string will almost surely be larger than the original in terms of bytes, but the encoding is reversible.
 //! The crate also provides functions to encode python code and wrap the result in a decoder that
 //! decodes and executes the encoded string. This way the file looks very different, but executes the same way as before.
 //! This lets you do the mother of all refactoring by converting your entire python program into a single line of code.
