6771b0fe231e6499415b8d1c69ecf967784c2f83
Johanna Sörngård <jsorngard@gmail.com>
2022-11-17 23:48:48 +0100

    Improve docstring

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # zalgo codec
 
-This crate is a port of the python code by [Scott Conner](https://github.com/DaCoolOne/DumbIdeas/tree/main/reddit_ph_compressor) to Rust. All credit goes to him.
+This crate is a port of the [Python code](https://github.com/DaCoolOne/DumbIdeas/tree/main/reddit_ph_compressor) by Scott Conner to Rust. All credit goes to him.
 
 Using the functions defined in this crate you can transform an ASCII string into a unicode string that is a single “character” wide.
 This string will be larger than the original in terms of bytes, but the encoding is reversible.
 The crate also provides functions to encode python code and wrap the result in a decoder that decodes and executes 
 the encoded string. This way the file looks very different, but executes the same way as before.
 This lets you do the mother of all refactoring by converting your entire python program into a single line of code. 
-Can not encode carriage returns, so files written on non-unix operating systems might not work.
+Can not encode carriage returns, so files written on non-unix operating systems might not work. The file encoding functions will attempt to encode files anyway by ignoring carriage returns.
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 //! This is a crate implementing the zalgo encoding and decoding functions
 //! [originally written in Python](https://github.com/DaCoolOne/DumbIdeas/tree/main/reddit_ph_compressor) by Scott Conner.
 //!
-//! Using the functions defined here you can transform an ASCII string into a unicode string that is a single
-//! "character" wide. This string will be larger than the original in terms of bytes,
-//! but the encoding is reversible. The crate also provides functions to encode python code and wrap the result in a decoder that
+//! Using the functions defined in this crate you can transform an ASCII string into a unicode string that is a single
+//! "character" wide. the encoding is reversible, but this string will be larger than the original in terms of bytes.
+//! The crate also provides functions to encode python code and wrap the result in a decoder that
 //! decodes and executes the encoded string. This way the file looks very different, but executes the same way as before.
 //! This lets you do the mother of all refactoring by converting your entire python program into a single line of code.
 //! Can not encode carriage returns, so files written on non-unix operating systems might not work. The file encoding
-//! functions will attempt to encode the file by ignoring carriage returns.
+//! functions will attempt to encode files anyway by ignoring carriage returns.
 //!
 //! Explanation:  
 //! Characters U+0300–U+036F are the combining characters for unicode Latin.
