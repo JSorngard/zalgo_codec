cff6a19f7894341f81d0bce4496bd05c3a020708
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-11-17 13:07:33 +0100

    Added docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-/// This is a crate implementing the zalgo encode and decode functions
+/// This is a crate implementing the zalgo compression and decompression functions
 /// originally written in Python by [Scott Conner](https://github.com/DaCoolOne/DumbIdeas/tree/main/reddit_ph_compressor).
+/// 
 /// Explanation by them:
 /// Characters U+0300–U+036F are the combining characters for unicode Latin.
 /// The fun thing about combining characters is that you can add as many of these characters
@@ -48,6 +49,8 @@ static UNKNOWN_CHAR_MAP: &[(u8, &str)] = &[
     (127, "DEL"),
 ];
 
+/// Searches through the static UNKNOWN_CHAR_MAP for the given key
+/// and returns the corresponding character if found.
 fn get_nonprintable_char_repr(key: u8) -> Option<&'static str> {
     UNKNOWN_CHAR_MAP
         .binary_search_by(|(k, _)| k.cmp(&key))
@@ -64,11 +67,11 @@ impl UnknownCharacterError {
         UnknownCharacterError {
             descriptor: if character < 128 {
                 match get_nonprintable_char_repr(character) {
-                    Some(repr) => format!("{line}: Cannot encode {repr} character"),
+                    Some(repr) => format!("{line}: cannot encode {repr} character"),
                     None => format!("{line}: ASCII {character}"),
                 }
             } else {
-                format!("{line}: Attempt to encode UTF8 character sequence (this program only can encode ascii non-control characters and newlines)")
+                format!("{line}: attempt to encode UTF8 character sequence (this program only can encode ascii non-control characters and newlines)")
             },
         }
     }
@@ -80,13 +83,23 @@ impl std::string::ToString for UnknownCharacterError {
     }
 }
 
-pub fn zalgo_compress(string_to_compress: &str) -> Result<String, String> {
+/// Takes in an ASCII string and "compresses" it to zalgo text
+/// using a reversible encoding scheme. The resulting string should
+/// only take up a single character space horizontally when displayed
+/// (though this can vary between platforms depending on how they deal with unicode).
+/// It can be decompressed to recover the original string using `zalgo_decode`.
+/// # Example
+/// ```
+/// # use zalgo_codec::zalgo_encode;
+/// assert_eq!(zalgo_encode("Zalgo").unwrap(), "É̺͇͌͏");
+/// ```
+pub fn zalgo_encode(string_to_compress: &str) -> Result<String, String> {
     let mut line = 1;
     let mut result: Vec<u8> = vec![b'E'];
 
     for c in string_to_compress.bytes() {
         if c == b'\r' {
-            return Err("Non-unix line endings detected".into());
+            return Err("non-unix line endings detected".into());
         }
 
         if c == b'\n' {
@@ -108,7 +121,15 @@ pub fn zalgo_compress(string_to_compress: &str) -> Result<String, String> {
     }
 }
 
-pub fn zalgo_decompress(compressed: &str) -> Result<String, String> {
+/// Takes in a string that was compressed by `zalgo_encode` and decompresses it.
+/// Returns an ASCII string.
+/// 
+/// # Example
+/// ```
+/// # use zalgo_codec::zalgo_decode;
+/// assert_eq!(zalgo_decode("É̺͇͌͏").unwrap(), "Zalgo");
+/// ```
+pub fn zalgo_decode(compressed: &str) -> Result<String, String> {
     let bytes: Vec<u8> = compressed
         .bytes()
         .skip(1)
@@ -131,35 +152,35 @@ mod tests {
     fn verify() {
         const TEST_STRING_1: &str = "the greatest adventure is going to bed";
         let out_string = std::str::from_utf8(b"E\xcd\x94\xcd\x88\xcd\x85\xcc\x80\xcd\x87\xcd\x92\xcd\x85\xcd\x81\xcd\x94\xcd\x85\xcd\x93\xcd\x94\xcc\x80\xcd\x81\xcd\x84\xcd\x96\xcd\x85\xcd\x8e\xcd\x94\xcd\x95\xcd\x92\xcd\x85\xcc\x80\xcd\x89\xcd\x93\xcc\x80\xcd\x87\xcd\x8f\xcd\x89\xcd\x8e\xcd\x87\xcc\x80\xcd\x94\xcd\x8f\xcc\x80\xcd\x82\xcd\x85\xcd\x84").unwrap();
-        assert_eq!(zalgo_compress(TEST_STRING_1).unwrap(), out_string);
+        assert_eq!(zalgo_encode(TEST_STRING_1).unwrap(), out_string);
 
         const TEST_STRING_2: &str =
             "I'll have you know I graduated top of my class in the Navy Seals";
         assert_eq!(
-            zalgo_decompress(&zalgo_compress(TEST_STRING_2).unwrap()).unwrap(),
+            zalgo_decode(&zalgo_encode(TEST_STRING_2).unwrap()).unwrap(),
             TEST_STRING_2
         );
 
-        const ASCII_TABLE: &str = r##"ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz1234567890 !"#$%&'()*+,-./:;<=>?@"##;
+        const ASCII_CHAR_TABLE: &str = r##"ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz1234567890 !"#$%&'()*+,-./:;<=>?@"##;
         assert_eq!(
-            zalgo_decompress(&zalgo_compress(ASCII_TABLE).unwrap()).unwrap(),
-            ASCII_TABLE
+            zalgo_decode(&zalgo_encode(ASCII_CHAR_TABLE).unwrap()).unwrap(),
+            ASCII_CHAR_TABLE
         );
     }
 
     #[test]
     fn newlines() {
-        assert_eq!(&zalgo_compress("\n").unwrap(), "Eͯ",);
+        assert_eq!(&zalgo_encode("\n").unwrap(), "Eͯ",);
         const TEST_STRING: &str = "The next sentence is true.\nThe previous sentence is false.";
         assert_eq!(
-            zalgo_decompress(&zalgo_compress(TEST_STRING).unwrap()).unwrap(),
+            zalgo_decode(&zalgo_encode(TEST_STRING).unwrap()).unwrap(),
             TEST_STRING,
         );
     }
 
     #[test]
     fn check_errors() {
-        assert!(zalgo_compress("We got the Ä Ö Å, you aint got the Ä Ö Å").is_err());
-        assert!(zalgo_compress("\t").is_err());
+        assert!(zalgo_encode("We got the Ä Ö Å, you aint got the Ä Ö Å").is_err());
+        assert!(zalgo_encode("\t").is_err());
     }
 }
