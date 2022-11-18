2e5845576955099e5767e5bccecd7a9a291f8b60
Alex Keizer <alex@keizer.dev>
2022-11-18 13:35:30 +0100

    Add a `zalgo_embed` macro
    It takes a string literal containing encoded data,
    parses the decoded data as rust code, and embed it in the source file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,40 @@
 
 pub use zalgo_codec_common::*;
+pub use zalgo_codec_macro::*;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_embed_function() {
+        let code = "fn add(x: i32, y: i32) -> i32 {x + y}";
+
+        let encoded = zalgo_encode(code).unwrap();
+        assert_eq!(encoded, "E͎͉͙͉̞͉͙͆̀́̈́̈́̈̀̓̒̌̀̀̓̒̉̀̍̀̓̒̀͛̀̋̀͘̚̚͘͝");
+
+        zalgo_embed!("E͎͉͙͉̞͉͙͆̀́̈́̈́̈̀̓̒̌̀̀̓̒̉̀̍̀̓̒̀͛̀̋̀͘̚̚͘͝");
+
+        // Now the `add` function is available
+        assert_eq!(add(10, 20), 30)
+    }
+
+    #[test]
+    fn test_embed_expression() {
+        let x = 20;
+        let y = -10;
+
+        let expr = "x + y";
+
+        let encoded = zalgo_encode(expr).unwrap();
+        println!("{}", encoded);
+        assert_eq!(encoded, "È͙̋̀͘");
+
+        // It works on expressions, too!
+        let z = zalgo_embed!("È͙̋̀͘");
+        assert_eq!(z, x + y);
+    }
+
+
+}
