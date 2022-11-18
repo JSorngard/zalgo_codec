2e4c35890de82af21991e1979cfcff0b1a58c710
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-11-18 17:59:37 +0100

    Formatting

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,4 @@
-//!A crate for converting an ASCII text string or file to a single unicode character.
+//! A crate for converting an ASCII text string or file to a single unicode character.
 //! Can also do the same to python code while still letting the code run as before by wrapping it in a decoder.
 //! This crate provides the non-macro functionality of the crate `zalgo-codec`.
 
