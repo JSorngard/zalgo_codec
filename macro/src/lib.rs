6780c8b124fffa7370ce984f1ae2ee9de33a0942
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-11-18 17:17:24 +0100

    Unified testing and added documentation to macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,9 +1,34 @@
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, LitStr};
 
-
-
 #[proc_macro]
+/// This macro decodes a Unicode string that has been encoded with `zalgo_encode`
+/// and passes the results on to the compiler.
+/// # Examples
+/// We can add arbitrary source code to our program.
+/// ```
+/// # use zalgo_codec_common::zalgo_encode;
+/// # use zalgo_codec_macro::zalgo_embed;
+/// // This line expands to the code
+/// // `fn add(x: i32, y: i32) -> i32 {x + y}`
+/// zalgo_embed!("E͎͉͙͉̞͉͙͆̀́̈́̈́̈̀̓̒̌̀̀̓̒̉̀̍̀̓̒̀͛̀̋̀͘̚̚͘͝");
+///
+/// // Now the `add` function is available
+/// assert_eq!(add(10, 20), 30);
+/// ```
+///
+/// It works on expressions too!
+/// ```
+/// # use zalgo_codec_common::zalgo_encode;
+/// # use zalgo_codec_macro::zalgo_embed;
+/// let x = 20;
+/// let y = -10;
+///
+/// // This macro is expanded to the code
+/// // `x + y`
+/// let z = zalgo_embed!("È͙̋̀͘");
+/// assert_eq!(z, x + y);
+/// ```
 pub fn zalgo_embed(encoded: TokenStream) -> TokenStream {
     let encoded = parse_macro_input!(encoded as LitStr).value();
 
