64c8b3a48df299f75586889187d902f691a5fe39
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-11-18 18:02:46 +0100

    Add docstring to macro crate

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,3 +1,6 @@
+//! This crate provides the macro part of `zalgo-codec` by defining the procedural macro
+//! `zalgo_embed!`.
+
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, LitStr};
 
