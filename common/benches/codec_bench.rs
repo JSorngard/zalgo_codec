420b91982f2df50f1f37e8112d4039821931d513
Johanna Sörngård <jsorngard@gmail.com>
2023-04-13 09:06:57 +0200

    Add file benchmarks

--- common/benches/codec_bench.rs
+++ common/benches/codec_bench.rs
@@ -1,10 +1,17 @@
+use std::{
+    fs,
+    path::PathBuf,
+    str::FromStr,
+    time::{Duration, Instant},
+};
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{
     distributions::{DistString, Distribution},
     seq::SliceRandom,
     thread_rng, Rng,
 };
-use zalgo_codec_common::{zalgo_decode, zalgo_encode};
+use zalgo_codec_common::{decode_file, encode_file, zalgo_decode, zalgo_encode};
 
 struct PrintableAsciiAndNewline;
 
@@ -32,5 +39,54 @@ fn bench_codec(c: &mut Criterion) {
     group.bench_function("decode", |b| b.iter(|| zalgo_decode(&encoded)));
 }
 
-criterion_group!(benches, bench_codec);
+fn bench_file_codec(c: &mut Criterion) {
+    const TEST_DIR: &str = "benches";
+    let string = PrintableAsciiAndNewline.sample_string(&mut thread_rng(), 100_000);
+
+    let mut orig_path = PathBuf::from_str(TEST_DIR).unwrap();
+    let mut encoded_path = orig_path.clone();
+    let mut decoded_path = orig_path.clone();
+
+    orig_path.push("original.txt");
+    encoded_path.push("encoded.txt");
+    decoded_path.push("decoded.txt");
+
+    fs::write(&orig_path, &string).unwrap();
+
+    let mut group = c.benchmark_group("file_codec");
+    group.bench_function("encode_file", |b| {
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::from_secs(0);
+            for _ in 0..iters {
+                let start = Instant::now();
+                encode_file(&orig_path, &encoded_path).unwrap();
+                let duration = start.elapsed();
+                fs::remove_file(&encoded_path).unwrap();
+                elapsed += duration;
+            }
+            elapsed
+        })
+    });
+
+    fs::write(&encoded_path, zalgo_encode(&string).unwrap()).unwrap();
+
+    group.bench_function("decode_file", |b| {
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::from_secs(0);
+            for _ in 0..iters {
+                let start = Instant::now();
+                decode_file(&encoded_path, &decoded_path).unwrap();
+                let duration = start.elapsed();
+                fs::remove_file(&decoded_path).unwrap();
+                elapsed += duration;
+            }
+            elapsed
+        })
+    });
+    fs::remove_file(orig_path).unwrap();
+    fs::remove_file(encoded_path).unwrap();
+    // fs::remove_file(decoded_path).unwrap();
+}
+
+criterion_group!(benches, bench_codec, bench_file_codec);
 criterion_main!(benches);
