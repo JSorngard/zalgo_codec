1c3e7353293d760879cce2901315c50c2140a89b
Johanna Sörngård <jsorngard@gmail.com>
2023-11-16 17:14:29 +0100

    Implemented encode, decode, copy and save as

--- codec/src/main.rs
+++ codec/src/main.rs
@@ -1,43 +1,48 @@
-use std::path::PathBuf;
-
-use iced::{self, Application, Element, Theme, executor::{self, Executor}, Command, widget::{text::Text, row}};
-use zalgo_codec_common::{ZalgoString, zalgo_decode};
-use anyhow::{anyhow, Result};
-
-#[derive(Debug, Clone)]
-enum InputSource {
-    TextField(String),
-    File(PathBuf),
-}
-
-#[derive(Debug, Clone)]
-enum OutputDestination {
-    TextField,
-    File(PathBuf),
-}
+use cli_clipboard::set_contents;
+use iced::{
+    self, executor,
+    widget::{button::Button, column, row, text::Text, text_input::TextInput, Space},
+    Application, Command, Element, Length, Theme,
+};
+use rfd::FileDialog;
+use zalgo_codec_common::{zalgo_decode, zalgo_encode, zalgo_wrap_python};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum CodecActionKind {
+enum GuiButton {
     Encode,
     Decode,
-    Wrap,
-    Unwrap,
+    Copy,
+    SaveAs,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum UserAction {
+    EditedInputText(String),
+    Pressed(GuiButton),
 }
 
 #[derive(Debug, Clone)]
-struct CodecAction {
-    input_source: InputSource,
-    output_dest: OutputDestination,
-    action_kind: CodecActionKind,
+enum ToplevelMessage {
+    CodecFinished(String),
+    User(UserAction),
 }
 
 #[derive(Debug)]
-enum ToplevelMessage {
-    BackendAction(CodecAction),
-    GuiAction,
+struct ZalgoCodecGui {
+    input_field: String,
+    output_field: String,
+    error_messages: Vec<String>,
 }
 
-struct ZalgoCodecGui();
+impl ZalgoCodecGui {
+    fn new() -> Self {
+        Self {
+            input_field: String::default(),
+            output_field: String::default(),
+            error_messages: Vec::default(),
+        }
+    }
+}
 
 impl Application for ZalgoCodecGui {
     type Executor = executor::Default;
@@ -46,7 +51,7 @@ impl Application for ZalgoCodecGui {
     type Message = ToplevelMessage;
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (ZalgoCodecGui(), Command::none())
+        (ZalgoCodecGui::new(), Command::none())
     }
 
     fn title(&self) -> String {
@@ -54,11 +59,92 @@ impl Application for ZalgoCodecGui {
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
-        Command::none()
+        match message {
+            Self::Message::CodecFinished(result) => {
+                self.output_field = result;
+                Command::none()
+            }
+            Self::Message::User(action) => match action {
+                UserAction::EditedInputText(text) => {
+                    self.input_field = text;
+                    Command::none()
+                }
+                UserAction::Pressed(GuiButton::Encode) => {
+                    let input = self.input_field.clone();
+                    Command::perform(async move { zalgo_encode(&input) }, |res| {
+                        ToplevelMessage::CodecFinished(res.map_or_else(|e| e.to_string(), |ok| ok))
+                    })
+                }
+                UserAction::Pressed(GuiButton::Decode) => {
+                    let input = self.input_field.clone();
+                    if input.is_empty() {
+                        Command::perform(
+                            async { String::from("the input string was empty") },
+                            |s| ToplevelMessage::CodecFinished(s),
+                        )
+                    } else {
+                        Command::perform(async move { zalgo_decode(&input) }, |res| {
+                            ToplevelMessage::CodecFinished(
+                                res.map_or_else(|e| e.to_string(), |ok| ok),
+                            )
+                        })
+                    }
+                }
+                UserAction::Pressed(GuiButton::Copy) => {
+                    if let Err(e) = set_contents(self.output_field.clone()) {
+                        self.error_messages.push(e.to_string());
+                    }
+                    Command::none()
+                }
+                UserAction::Pressed(GuiButton::SaveAs) => {
+                    if let Some(path) = FileDialog::new().set_file_name("zalgo.txt").save_file() {
+                        if let Err(e) = std::fs::write(path, &self.output_field) {
+                            self.error_messages.push(e.to_string());
+                        }
+                    }
+                    Command::none()
+                }
+            },
+        }
     }
 
     fn view(&self) -> Element<'_, Self::Message> {
-        row![Text::new("Hello, world!")].into()
+        column![
+            row![
+                column![
+                    TextInput::new("Type or paste text here!", &self.input_field)
+                        .on_input(|s| ToplevelMessage::User(UserAction::EditedInputText(s)))
+                        .on_paste(|s| ToplevelMessage::User(UserAction::EditedInputText(s))),
+                    Button::new("Encode").on_press(ToplevelMessage::User(UserAction::Pressed(
+                        GuiButton::Encode
+                    ))),
+                    Button::new("Decode").on_press(ToplevelMessage::User(UserAction::Pressed(
+                        GuiButton::Decode
+                    ))),
+                ]
+                .width(Length::FillPortion(3)),
+                Space::with_width(Length::Fill),
+                column![
+                    Text::new(&self.output_field),
+                    Button::new("Copy")
+                        .on_press(ToplevelMessage::User(UserAction::Pressed(GuiButton::Copy))),
+                    Button::new("Save as").on_press(ToplevelMessage::User(UserAction::Pressed(
+                        GuiButton::SaveAs
+                    ))),
+                ]
+                .width(Length::FillPortion(3)),
+            ]
+            .width(Length::Fill),
+            Text::new(
+                self.error_messages
+                    .iter()
+                    .rev()
+                    .fold(String::new(), |toast, msg| format!("{toast}\n{msg}"))
+            )
+            .width(Length::Fill),
+        ]
+        .width(Length::Fill)
+        .into()
     }
 }
 
