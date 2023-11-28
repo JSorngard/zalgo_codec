dcab8cb986ae0e7dc97f81763d80a69e3e618a51
Johanna Sörngård <jsorngard@gmail.com>
2023-11-28 15:11:30 +0100

    Same text for std feature

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -51,7 +51,7 @@
 //!
 //! # Features
 //! 
-//! `std`: implements the [`std::error::Error`] trait for the provided [`Error`] type.
+//! `std`: links the standard library and uses it to implement the [`std::error::Error`] trait for the provided [`Error`] type.
 //! If this feature is not enabled the library is `no_std`, but still uses the `alloc` crate.
 //!
 //! `serde`: implements the [`Serialize`](serde::Serialize) and [`Deserialize`](serde::Deserialize) traits for [`ZalgoString`].
