71abe26720103e1a8a2f60c8fb817870a1fe4277
Johanna Sörngård <jsorngard@gmail.com>
2023-12-19 14:07:08 +0100

    Add as_combining_chars

--- common/src/zalgo_string.rs
+++ common/src/zalgo_string.rs
@@ -386,6 +386,20 @@ impl ZalgoString {
         self.0.push_str(combining_chars);
         Ok(())
     }
+
+    /// Returns a string slice of just the combining characters of the `ZalgoString` without the inital 'E'.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use zalgo_codec_common::{Error, ZalgoString};
+    /// let zs = ZalgoString::new("Hi")?;
+    /// assert_eq!(zs.as_combining_chars(), "\u{328}\u{349}");
+    /// # Ok::<(), Error>(())
+    /// ```
+    pub fn as_combining_chars(&self) -> &str {
+        self.0.split_at(1).1
+    }
 }
 
 /// An iterator over the decoded bytes of a [`ZalgoString`].
