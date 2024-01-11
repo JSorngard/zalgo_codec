13d770cacba522feb914647eda5fc63b066c4366
Johanna Sörngård <jsorngard@gmail.com>
2024-01-11 14:02:20 +0100

    Move zalgostring iterators to their own file

--- common/src/zalgo_string/mod.rs
+++ common/src/zalgo_string/mod.rs
@@ -5,10 +5,12 @@
 //! It can be decoded in-place and the encoded information in other ZalgoStrings can be pushed
 //! onto it.
 
+mod iterators;
+
 use crate::{decode_byte_pair, fmt, zalgo_encode, Error};
+use iterators::{DecodedBytes, DecodedChars};
 
 use core::{
-    iter::{ExactSizeIterator, FusedIterator},
     ops::{Index, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive},
     slice::SliceIndex,
 };
@@ -206,7 +208,7 @@ impl ZalgoString {
     /// ```
     #[inline]
     pub fn decoded_chars(&self) -> DecodedChars<'_> {
-        DecodedChars(self.decoded_bytes())
+        DecodedChars::new(self)
     }
 
     /// Converts `self` into a `String`.
@@ -312,7 +314,7 @@ impl ZalgoString {
     /// ```
     #[inline]
     pub fn decoded_bytes(&self) -> DecodedBytes<'_> {
-        DecodedBytes(self.as_combining_chars().bytes())
+        DecodedBytes::new(self)
     }
 
     /// Converts `self` into a byte vector.
@@ -596,124 +598,6 @@ impl ZalgoString {
 
     // endregion: length manipulation methods
 }
-
-// region: Iterator impls
-
-/// An iterator over the decoded bytes of a [`ZalgoString`].
-///
-/// This struct is obtained by calling the [`decoded_bytes`](ZalgoString::decoded_bytes) method on a [`ZalgoString`].
-/// See its documentation for more.
-#[derive(Debug, Clone)]
-#[must_use = "iterators are lazy and do nothing unless consumed"]
-pub struct DecodedBytes<'a>(core::str::Bytes<'a>);
-
-impl<'a> Iterator for DecodedBytes<'a> {
-    type Item = u8;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0
-            .next()
-            .zip(self.0.next())
-            .map(|(odd, even)| decode_byte_pair(odd, even))
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let left = self.0.size_hint().0 / 2;
-        (left, Some(left))
-    }
-
-    #[inline]
-    fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.0
-            .nth(2 * n)
-            .zip(self.0.next())
-            .map(|(odd, even)| decode_byte_pair(odd, even))
-    }
-
-    #[inline]
-    fn last(mut self) -> Option<Self::Item> {
-        self.0
-            .len()
-            // Check if there are at least two bytes left
-            .checked_sub(2)
-            .and_then(|l| {
-                self.0
-                    // Get the next to last,
-                    .nth(l)
-                    // and the last
-                    .zip(self.0.next())
-                    // and decode them
-                    .map(|(odd, even)| decode_byte_pair(odd, even))
-            })
-    }
-
-    #[inline]
-    fn count(self) -> usize {
-        self.0.count() / 2
-    }
-}
-
-impl<'a> DoubleEndedIterator for DecodedBytes<'a> {
-    #[inline]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.0
-            .next_back()
-            .zip(self.0.next_back())
-            .map(|(even, odd)| decode_byte_pair(odd, even))
-    }
-}
-
-impl<'a> FusedIterator for DecodedBytes<'a> {}
-impl<'a> ExactSizeIterator for DecodedBytes<'a> {}
-
-/// An iterator over the decoded characters of a [`ZalgoString`].
-///
-/// This struct is obtained by calling the [`decoded_chars`](ZalgoString::decoded_chars) method on a [`ZalgoString`].
-/// See it's documentation for more.
-#[derive(Debug, Clone)]
-#[must_use = "iterators are lazy and do nothing unless consumed"]
-pub struct DecodedChars<'a>(DecodedBytes<'a>);
-
-impl<'a> Iterator for DecodedChars<'a> {
-    type Item = char;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(char::from)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
-    }
-
-    #[inline]
-    fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.0.nth(n).map(char::from)
-    }
-
-    #[inline]
-    fn count(self) -> usize {
-        self.0.count()
-    }
-
-    #[inline]
-    fn last(self) -> Option<Self::Item> {
-        self.0.last().map(char::from)
-    }
-}
-
-impl<'a> DoubleEndedIterator for DecodedChars<'a> {
-    #[inline]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.0.next_back().map(char::from)
-    }
-}
-
-impl<'a> FusedIterator for DecodedChars<'a> {}
-impl<'a> ExactSizeIterator for DecodedChars<'a> {}
-
-// endregion: Iterator impls
 
 // region: Addition impls
 
