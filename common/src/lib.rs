a5ae49012ef3d26450a2e38da372e6b0cfe4ecb3
Johanna Sörngård <jsorngard@gmail.com>
2024-01-24 16:27:49 +0100

    Clarify docstring

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -122,8 +122,8 @@ pub use zalgo_string::ZalgoString;
 /// # Ok::<(), Error>(())
 /// ```
 /// Can not encode ASCII control characters except newlines.
-/// Notably this means that this function can not encode tab characters or carriage returns,
-/// which are present in e.g. line endings on Windows:
+/// Notably this means that this function can not encode tab characters or carriage returns.
+/// Carriage returns are present in e.g. line endings on Windows:
 /// ```
 /// # use zalgo_codec_common::zalgo_encode;
 /// assert!(zalgo_encode("CRLF\r\n").is_err());
