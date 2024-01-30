0b6b353245a92be0295649b54d6a699b0bee3b8e
Johanna Sörngård <jsorngard@gmail.com>
2024-01-30 12:47:59 +0100

    Make Error a struct instead of an enum

--- common/src/error.rs
+++ common/src/error.rs
@@ -2,24 +2,33 @@
 
 use core::fmt;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 /// The error returned by [`zalgo_encode`](crate::zalgo_encode), [`ZalgoString::new`](crate::ZalgoString::new), and [`zalgo_wrap_python`](crate::zalgo_wrap_python)
 /// if they encounter a byte they can not encode.
 ///
 /// Only implements the [`Error`](std::error::Error) trait if the `std` feature is enabled.
-pub enum Error {
-    /// Represents a valid ASCII character that is outside of the encodable set.
-    /// The first `u8` in the variant is the byte value of the character, the first `usize`
-    /// is the 1-indexed line number where the character occured, the second `usize` is
-    /// the 1-indexed column in which the character occured and the `&str` is a description
-    /// of the character.
-    UnencodableAscii(u8, usize, usize, &'static str),
-    /// Represents some other character.
-    /// The two `usize`s represent the same thing as in the `UnencodableAscii` variant,
-    NotAscii(char, usize, usize),
+pub struct Error {
+    unencodable_character: char,
+    line: usize,
+    column: usize,
+    index: usize,
 }
 
 impl Error {
+    pub(crate) const fn new(
+        unencodable_character: char,
+        line: usize,
+        column: usize,
+        index: usize,
+    ) -> Self {
+        Self {
+            unencodable_character,
+            line,
+            column,
+            index,
+        }
+    }
+
     /// Returns the 1-indexed line number of the line on which the unencodable byte occured.
     ///
     /// # Examples
@@ -32,9 +41,7 @@ impl Error {
     #[inline]
     #[must_use = "the method returns a new value and does not modify `self`"]
     pub const fn line(&self) -> usize {
-        match self {
-            Self::UnencodableAscii(_, line, _, _) | Self::NotAscii(_, line, _) => *line,
-        }
+        self.line
     }
 
     /// Returns the 1-indexed column where the unencodable byte occured.
@@ -50,9 +57,7 @@ impl Error {
     #[inline]
     #[must_use = "the method returns a new value and does not modify `self`"]
     pub const fn column(&self) -> usize {
-        match self {
-            Self::UnencodableAscii(_, _, column, _) | Self::NotAscii(_, _, column) => *column,
-        }
+        self.column
     }
 
     /// Returns the unencodable character that caused the error.
@@ -82,73 +87,31 @@ impl Error {
     #[inline]
     #[must_use = "the method returns a new value and does not modify `self`"]
     pub const fn char(&self) -> char {
-        match self {
-            Self::UnencodableAscii(byte, _, _, _) => *byte as char,
-            Self::NotAscii(char, _, _) => *char,
-        }
+        self.unencodable_character
     }
 
-    /// If the unencodable character is an ASCII character
-    /// this function returns a representation of it.
+    /// Returns the index of the string where the unencodable character occured
     ///
-    /// # Examples
+    /// # Example
     ///
     /// ```
     /// # use zalgo_codec_common::zalgo_encode;
-    /// assert_eq!(zalgo_encode("\r").map_err(|e| e.representation()), Err(Some("Carriage Return")));
-    /// assert_eq!(zalgo_encode("❤️").map_err(|e| e.representation()), Err(None));
+    /// assert_eq!(zalgo_encode("ab\ncdë").map_err(|e| e.index()), Err(5));
     /// ```
     #[inline]
     #[must_use = "the method returns a new value and does not modify `self`"]
-    pub const fn representation(&self) -> Option<&'static str> {
-        match self {
-            Self::UnencodableAscii(_, _, _, repr) => Some(*repr),
-            Self::NotAscii(_, _, _) => None,
-        }
-    }
-
-    /// Returns whether the error is the [`NotAscii`](Error::NotAscii) variant.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use zalgo_codec_common::{Error, zalgo_encode};
-    /// assert_eq!(zalgo_encode("Blå").map_err(|e| e.is_not_ascii()), Err(true));
-    /// ```
-    #[inline]
-    #[must_use = "the method returns a new value and does not modify `self`"]
-    pub const fn is_not_ascii(&self) -> bool {
-        matches!(self, Self::NotAscii(_, _, _))
-    }
-
-    /// Returns whether the error is the [`UnencodableAscii`](Error::UnencodableAscii) variant.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use zalgo_codec_common::{Error, zalgo_encode};
-    /// assert_eq!(zalgo_encode("true\rfalse").map_err(|e| e.is_unencodable_ascii()), Err(true));
-    /// ```
-    #[inline]
-    #[must_use = "the method returns a new value and does not modify `self`"]
-    pub const fn is_unencodable_ascii(&self) -> bool {
-        matches!(self, Self::UnencodableAscii(_, _, _, _))
+    pub const fn index(&self) -> usize {
+        self.index
     }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::UnencodableAscii(byte, line, column, repr) => write!(
-                f,
-                "line {line} at column {column}: can not encode ascii '{repr}' character with byte value {byte}"
-            ),
-            Self::NotAscii(char, line, column) => write!(
-                f,
-                "line {line} at column {column}: can not encode non-ascii character '{char}' (U+{:X})",
-                u32::from(*char)
-            ),
-        }
+        write!(
+            f,
+            "line {} at column {}: can not encode character {:?}",
+            self.line, self.column, self.unencodable_character
+        )
     }
 }
 
@@ -161,19 +124,24 @@ mod test {
 
     #[test]
     fn test_error() {
-        let err = Error::NotAscii('å', 1, 7);
-        assert!(err.is_not_ascii());
+        let err = Error {
+            line: 1,
+            column: 7,
+            unencodable_character: 'å',
+            index: 6,
+        };
         assert_eq!(err.char(), 'å');
         assert_eq!(err.line(), 1);
         assert_eq!(err.column(), 7);
-        assert_eq!(err.representation(), None);
 
-        let err2 = Error::UnencodableAscii(13, 1, 2, "Carriage Return");
-        assert!(err2.is_unencodable_ascii());
-        assert_eq!(err2.char(), '\r');
+        let err2 = Error {
+            line: 1,
+            column: 2,
+            unencodable_character: '\r',
+            index: 1,
+        };
         assert_eq!(err2.line(), 1);
         assert_eq!(err2.column(), 2);
-        assert_eq!(err2.representation(), Some("Carriage Return"));
 
         assert_ne!(err, err2);
         let err3 = err;
